33f01d2b7159d2800be282c219b86c2aa2f15fb0
Vinicius Linck <vinnylinck@gmail.com>
2014-01-24 12:49:08 -0200

    Added first directive

---
+++ scripts/tapAnim.js
@@ -0,0 +1,21 @@
+/* global angular */
+angular.module("ngtlantic").directive("tapAnim", function () {
+    
+    return function (scope, elm, attrs) {
+
+        elm.addClass("animated");
+        
+        elm.bind("click", function () {
+            
+            if (elm.hasClass("expanded")) {
+                elm.removeClass("expanded");
+            } else {
+                elm.addClass("expanded");
+            }
+
+        });
+        
+        
+    };
+    
+});
